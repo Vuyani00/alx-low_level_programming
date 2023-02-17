46feba6159e8d4959e173de6c3cee1a5a7f5efb8
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-17 09:48:05 -0800

    C program that prints to standard error and returns 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always 1 (Failure)
+  */
+int main(void)
+{
+	/**
+	  *fprintf function allows us to print to the standard error
+	  */
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
+
