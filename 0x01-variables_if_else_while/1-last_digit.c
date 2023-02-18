27abb1659300a6d4ed2de594362b0fe1bbe344d3
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-18 04:37:50 -0800

    Program that calculate the last digit of n,  * then check if is greater than 5, or equals 0, or less than 6 but not 0.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**main - Entry point
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+	/* your code goes there */
+	if (n > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %i is %i is zero\n", n, last);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	}
+
+	return (0);
+}
