4ada8ab45cdbed055e706f88c9a89c715113fe20
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-18 10:04:45 -0800

    C program that print the first ten natural numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Description: Function that prints out the 1st 10 natuaral numbers
+  *
+  *Return: always 0 (Success)
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d",a);
+	}
+
+	printf("\n");
+
+	return (0);
+}
