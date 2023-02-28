d476603b8550013d02f06dd788e8c7a2aa199772
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-27 23:25:50 -0800

    Function that prints out putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**main - Entry point
+  *
+  *Description: Function that prints putchar
+  *
+  *Return: 0 (Success)
+  */
+
+int main ()
+{
+	_putchar("putchar");
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar (char* c)
+{
+	return c;
+}
+
+#endif
