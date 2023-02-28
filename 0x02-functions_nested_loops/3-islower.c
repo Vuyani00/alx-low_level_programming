bc21c4948b8b1f10bbd7149ce19d8970d1fa1a04
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-28 09:03:22 -0800

    Function that detects lowwercase characters and returns 1 if the conditions are met

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  *_islower - checks for lowercase letters
+  *
+  *Descrption: checks whether a character is lowercase/uppercase
+  *
+  *Return: 0 (Success)
+  */
+
+int _islower(int c)
+{
+	char alpha;
+	/*c - integer*/
+	int c = 0;
+
+	if (islower(alpha) > c)
+	{
+		return (1);
+	} else if (alpha == c)
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *
+  *Dsecription: checks wheteher a character is lowercase/uppercase 
+  *
+  *Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int r;
+
+	_islower(r);
+
+	return (0);
+}
