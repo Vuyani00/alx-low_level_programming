c42a3440e65a64898137a7b5672c944056b1dbe6
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-27 23:34:09 -0800

    Function that prints out putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,8 +9,14 @@
 
 int main ()
 {
-	_putchar("putchar");
-	putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
