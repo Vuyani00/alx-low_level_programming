4a0eb08c63125f9736fe7f10658a7af425de9661
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-28 00:07:18 -0800

    Function that prints out the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *print_alphabet -
+  *
+  *Description: Function that prints the alphabet
+  *
+  *Return: 0 (Success)
+  */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+	char omega = 'z';
+
+	while (alpha <= omega)
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+
+	_putchar('\n');
+
+}
