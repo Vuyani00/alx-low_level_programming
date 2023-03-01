67f564356fab205b49e9317c8bec84769b7fdb91
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-28 23:34:11 -0800

    Prints the sum of numbers that are multiples of 3 & 5 between 1024

---
+++ 0x02-functions_nested_loops/101-main.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *Description: main function
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *Description: sums the multiples of 3 & 5 between 0 & 1024
+  *Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n <= 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d", sum);
+
+	return (0);
+}
