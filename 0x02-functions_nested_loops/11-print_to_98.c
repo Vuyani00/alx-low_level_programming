926984c398a54b6e67b9af8ed5f59eb97ced07cd
Vuyani00 <nkosivuyani03@gmail.com>
2023-02-28 22:47:11 -0800

    Function that counts up/down to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  *print_to_98 - prints to 98
+  *
+  *@n: input
+  *
+  *Description: prints from the given number till it reaches 98
+  *
+  *Return: 0 (Success)
+  */
+
+void print_to_98(int n)
+{
+
+	if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+			if (n == 98)
+			{
+				continue;
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+		else 
+		{
+			while (n <= 98)
+			{
+				printf("%d, ", n);
+				n++;
+				if (n == 98)
+				{
+					continue;
+					printf(", ");
+				}
+			}
+			printf("\n");
+		}
+}
